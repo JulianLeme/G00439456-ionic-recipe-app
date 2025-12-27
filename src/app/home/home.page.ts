7afbfca79dfba830313590fab7923344c1129292
Julianderson Leme <g00439456@atu.ie>
2025-12-27 16:03:55 +0000

    Home page: dynamic recipe list using *ngFor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonList, IonItem , IonLabel } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonList, IonItem, IonLabel],
+  standalone : true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonList, IonItem, IonLabel, CommonModule],
 })
 export class HomePage {
+recipes = [
+    {
+      name: 'Sushi',
+      description: 'Classic Japanese'
+    },
+    {
+      name: 'Lasagna',
+      description: 'Classic Italian'
+    }
+  ];
+
+
+
+
   constructor() {}
 }
